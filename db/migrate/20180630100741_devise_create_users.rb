2a61ecdb516b3e34710defcb7c91e388a7ed8eca
藤田澄智 <sumitoshi@fujitakiyoshisatoshinoMacBook-puro.local>
2018-06-30 21:45:57 +0900

    Setup modal
    setup
    ・modal
    ・bootstrap3

--- db/migrate/20180630100741_devise_create_users.rb
+++ db/migrate/20180630100741_devise_create_users.rb
@@ -3,6 +3,20 @@
 class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
+      t.string  :first_name,         null: false
+      t.string  :last_name,          null: false
+      t.integer :gender
+      t.string  :birthday
+      t.string  :phone_number
+      t.string  :image
+      t.string  :display_language
+      t.string  :currency
+      t.string  :address
+      t.string  :self_introduction
+      t.string  :school_name
+      t.string  :company_name
+      t.string  :standard_time
+      t.string  :business_email
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
